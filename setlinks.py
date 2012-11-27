e450213165ff846fcedc9078cbd6bf6243bd64e3
Ryan Mendivil <rsmendivil@gmail.com>
2012-11-26 16:10:07 -0900

    setlinks: Added script to set links

---
+++ setlinks.py
@@ -0,0 +1,24 @@
+from os      import symlink
+from os.path import islink, expanduser, exists
+from shutil  import move
+
+configs = ["vimrc", "xmonad", "xmobarrc"]
+home = expanduser("~")
+
+for config in configs:
+    home_file = home + "/." + config
+    config_file = home + "/.config/" + config
+
+    if islink(home_file):
+        print config + ": Already linked"
+    else:
+        if exists(home_file):
+            move(home_file, config_file)
+            symlink(config_file, home_file)
+            print config + ": Moved existing file to .config"
+        elif exists(config_file):
+            symlink(config_file, home_file)
+            print config + ": Set link to existing file in .config"
+        else:
+            print config + ": Couldn't find file"
+
