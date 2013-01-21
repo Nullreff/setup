b96d9540738d7caaf77d7f26e6723d56a35867f6
Ryan Mendivil <rsmendivil@gmail.com>
2013-01-20 20:36:14 -0900

    conky: Add conky and lua

--- setlinks.py
+++ setlinks.py
@@ -2,7 +2,7 @@ from os      import symlink
 from os.path import islink, expanduser, exists
 from shutil  import move
 
-configs = ["vimrc", "xmonad", "xmobarrc", "gitconfig"]
+configs = ["vimrc", "xmonad", "xmobarrc", "gitconfig", "conkyrc", "lua"]
 home = expanduser("~")
 
 for config in configs:
