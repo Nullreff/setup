c26cd4bb743da8b52b9225fcba0e7b65ed736bac
Ryan Mendivil <ryan@nullreff.net>
2015-07-13 11:55:33 -0700

    xmonad: Float steam window

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -63,6 +63,10 @@ myLayout = lessBorders OnlyFloat $ avoidStruts $ named "Tabs" myTabbed
 myManageHook = composeAll
     [ className =? "Vncviewer"     --> doFloat
     , className =? "Xfce4-notifyd" --> doIgnore
+    , className =? "Steam" --> doFloat
+    , className =? "steam" --> doFullFloat -- bigpicture-mode
+    , className =? "MainThrd" --> doFloat
+    , title =? "plasma-desktop" --> doIgnore
     , isFullscreen                 --> (doF W.focusDown <+> doFullFloat)
     ]
 
