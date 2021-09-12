0fdc236eb13a374a8f698bcc5c43a87a538677b5
Nullreff <git@nullreff.net>
2021-09-12 02:07:39 -0700

    Add GDK_BACKEND wayland to fish config

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,9 @@ set PATH ~/.cargo/bin $PATH
 set PATH /snap/bin $PATH
 set PATH ~/.emacs.d/bin $PATH
 
+# Wayland fixes, disable if you're not using it
+set GDK_BACKEND wayland
+
 set fish_greeting ""
 setenv EDITOR vim
 
